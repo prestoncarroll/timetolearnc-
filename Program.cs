586bb9c1fe18b4e14f4f4a6a4ac7a835e5321b17
Preston Carroll <plutusinc1@yahoo.com>
2020-05-03 15:19:04 -0500

    variables declared

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            String name = "john";
+            int age = 23;
+            Console.WriteLine(name + " is " + age);
+            
+
+
+        }
+    }
+}
