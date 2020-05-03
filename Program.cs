b88585fbd24abde61630fc919de313e10db49f19
Preston Carroll <plutusinc1@yahoo.com>
2020-05-03 16:37:24 -0500

    get user input

--- Program.cs
+++ Program.cs
@@ -7,11 +7,56 @@ namespace HelloWorld
         static void Main(string[] args)
         {
 
+            Console.Write("Enter your name: ");
 
-            String name = "john";
-            int age = 23;
-            Console.WriteLine(name + " is " + age);
-            
+            string name = Console.ReadLine();
+            Console.WriteLine("hello " + name);
+
+
+
+
+
+            Console.ReadLine();
+
+
+            // string phrase = "my academy";
+            // int num = 23;
+            // num++;
+
+
+
+            // Console.WriteLine(num);
+            // Console.WriteLine(phrase.Length);
+            // Console.WriteLine(phrase.ToUpper());
+            // Console.WriteLine(phrase.Contains("academy")); //outputs true
+            // Console.WriteLine(phrase[0]); //outputs m
+            // Console.WriteLine(phrase.IndexOf("academy")); //outputs 3
+
+            // Console.ReadLine();
+
+            //----------------------------------DECLARING VARIABLES
+            // String name = "john";
+            // int age = 23;
+            // Console.WriteLine(name + " is " + age);
+
+
+
+            //----------------------------------------------------------------
+            // string name = "john";
+            // char grade = 'A'; //ONLY ONE CHARACTER
+            // int age = 23;
+            // double gpa = 3.3; //DECIMAL
+            // bool isMale = true;
+
+            // if (isMale == true)
+            // {
+            //     Console.WriteLine(name + " got an " + grade + " in the class." + " he is " + age + " and his gpa is " + gpa );
+            // }
+
+
+            // Console.ReadLine();
+
+
 
 
         }
