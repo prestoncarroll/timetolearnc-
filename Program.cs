e675135db90a2bef698d7a5a5e5bdd8a38616730
Preston Carroll <plutusinc1@yahoo.com>
2020-05-03 17:41:25 -0500

    working with arrays

--- Program.cs
+++ Program.cs
@@ -7,18 +7,96 @@ namespace HelloWorld
         static void Main(string[] args)
         {
 
-            Console.Write("Enter your name: ");
 
-            string name = Console.ReadLine();
-            Console.WriteLine("hello " + name);
+            int[] luckyNumbers = { 4, 8, 15, 77, 99, 5 };
+            string[] names = { "jane", "julia", "james" };
 
+            Console.WriteLine(luckyNumbers[2] + names[1]); //15julia
 
-
+            Console.WriteLine(luckyNumbers.Length);//6
+            Console.WriteLine(luckyNumbers[2]); //15
 
 
             Console.ReadLine();
 
 
+
+
+            //---------------------------------------------------------------
+            // string color, pluralNoun, celebrity;
+
+            // Console.Write("enter a color: ");
+            // color = Console.ReadLine();
+
+            // Console.Write("enter a plural noun ");
+            // pluralNoun = Console.ReadLine();
+
+            // Console.Write("enter a celebrity: ");
+            // celebrity = Console.ReadLine();
+
+
+            // Console.WriteLine("roses are " + color);
+            // Console.WriteLine(pluralNoun + " are blue");
+            // Console.WriteLine("i love " + celebrity);
+
+
+
+
+
+            // -------------------------------------------------------------
+            // Console.Write("enter a number:");
+
+            // double num1 = Convert.ToDouble(Console.ReadLine());
+
+            // Console.Write("enter a number:");
+
+            // double num2 = Convert.ToDouble(Console.ReadLine());
+
+            // Console.WriteLine(num1 + num2);
+
+
+            // Console.ReadLine();
+
+
+
+
+
+
+
+            //converting string to a number
+            // int num = Convert.ToInt32("45");
+            // Console.WriteLine(num + 6);
+
+
+            // Console.ReadLine();
+
+
+
+
+
+
+            // ------------------------------------------------------USER INPUT
+            // Console.Write("Enter your name: ");
+            // string name = Console.ReadLine();
+            // Console.Write("Enter your age: ");
+            // string age = Console.ReadLine();
+
+            // Console.WriteLine("hello " + name + " you are " + age);
+
+
+
+
+
+
+            // Console.ReadLine();
+
+
+
+
+
+
+
+            //---------------------------------------------------------------
             // string phrase = "my academy";
             // int num = 23;
             // num++;
